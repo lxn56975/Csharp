e33481ed6aa8df3295602efda3ac0456c6b47785
刘晓宁 <lxn56975@163.com>
2012-11-29 11:37:20 +0800

    add

---
+++ FinalWork/HotelReservationSystem/DAL/Customers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Model;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    class Customers
+    {
+        public static DataTable GetAllUser()
+        {
+            DataBase db = new DataBase();
+            return db.GetDataTable("Select * from Customer");
+        }
+        public static void Save()
+        {
+            DataBase db = new DataBase();
+            string sql = "insert ";
+        }
+    }
+}
---
+++ FinalWork/HotelReservationSystem/Model/Customer.cs
@@ -0,0 +1,63 @@
+﻿                 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class Customer
+    {
+        private int _customerID;
+        private string _customerName;
+        private string _customerPassword;
+        private string _customerTel;
+        private string _customerAddress;
+        private string _customerShenFenID;
+        private string _customerEmail;
+
+
+
+        public int CustomerID
+        {
+            get { return this._customerID; }
+            set {this._customerID=value;}
+        }
+        public string CustomerName
+        {
+            get { return this._customerName; }
+            set { this._customerName = value; }
+        }
+        public string CustomerPassword
+        {
+            get { return this._customerPassword; }
+            set { this._customerPassword = value;}
+        }
+        public string CustomerTel
+        {
+            get { return this._customerTel; }
+            set { this._customerTel = value; }
+        }
+        public string CustomerAddress
+        {
+            get { return this.CustomerAddress; }
+            set { this.CustomerAddress = value; }
+        }
+        public string CustomerShenFenID
+        {
+            get { return this._customerShenFenID; }
+            set { this._customerShenFenID = value; }
+        }
+        public string CustomerEmail
+        {
+            get { return this._customerEmail; }
+            set { this._customerEmail = value; }
+        }
+        public Customer() { }
+        public Customer(int customerid, string customername, string customerpassword)
+        {
+            this._customerID = customerid;
+            this._customerName = customername;
+            this._customerPassword = customerpassword;
+        }
+    }
+}
